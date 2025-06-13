97e4a44125fff3df421fc4060e4ca595d1e9f801
INDJokerGamma <piyushdawkhare0000@gmail.com>
2025-06-13 14:19:33 +0530

    connecting the full board page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import JoinForm from "./Pages/JoinForm";
+import FullBoard from "./Pages/FullBoard";
 import Standout from "./Pages/events/Standout";
 import Aspire from "./Pages/events/Aspire";
 import Ideathon from "./Pages/events/Ideathon";
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/JoinForm" element={<JoinForm />} />
+        <Route path="/FullBoard" element={<FullBoard />} />
         <Route path="/events/standout" element={<Standout />} />
         <Route path="/events/aspire" element={<Aspire />} />
         <Route path="/events/nitigya" element={<Nitigya />} />
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,6 +10,7 @@ import Navbar from '../components/Navbar';
 
 
 
+
 export default function Home() {
   const prudenceRef = useRef(null);
 
