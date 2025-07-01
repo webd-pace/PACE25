39cb0ef9bd1a858029e1a3ce8ee59a1a262977f7
INDJokerGamma <piyushdawkhare0000@gmail.com>
2025-07-01 20:26:31 +0530

    changes required for mobilephone screen

--- src/components/PopupMessage.jsx
+++ src/components/PopupMessage.jsx
@@ -16,7 +16,8 @@ const PopupMessage = ({ message, onClose, duration = 5000 }) => {
   return (
     <>
       {/* Overlay */}
-      <div className="fixed top-0 left-0 w-full h-full bg-white bg-opacity-80 z-[9998]"></div>
+        <div className="fixed inset-0 z-[9998] bg-white bg-opacity-80 flex items-center justify-center"/>
+        <div className="bg-gray-900 text-white px-6 py-4 rounded-xl shadow-2xl border border-yellow-400 animate-popup-slide z-[9999] max-w-[90%] text-center"/>
 
       {/* Centered Popup */}
       <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
