bfdc39778de6e160794424172bbb2a3482afb559
INDJokerGamma <piyushdawkhare0000@gmail.com>
2025-07-06 19:24:52 +0530

    aa

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { motion } from "framer-motion";
--- src/Pages/PrudenceRegistration/PrudenceRegistration.jsx
+++ src/Pages/PrudenceRegistration/PrudenceRegistration.jsx
@@ -8,20 +8,20 @@ const PrudenceRegistration = () => {
   return (
     <>
       <Navbar />
-      <div className="bg-black text-white min-h-screen flex flex-col items-center justify-center py-20 px-4">
-        <h1 className="text-5xl font-extrabold text-yellow-400 mb-6 font-cinzel-decorative">
+      <div className="bg-black text-white min-h-screen flex flex-col items-center justify-center py-20 px-4 text-center">
+        <h1 className="text-3xl md:text-5xl font-extrabold text-yellow-400 mb-4 font-cinzel-decorative">
           PRUDANCE 2K25
         </h1>
-        <p className="text-5xl font-extrabold text-yellow-400 mb-6 ">
-          Stay Tuned for the Upcoming Events !!
+        <p className="text-0.5xl md:text-3xl font-semibold text-yellow-400 mb-6">
+          Exciting things are coming <br/>— stay tuned for what’s next! 
         </p>
 
         {/* Prudence Registration */}
         {/* <PreudenceRegistrationbase/> */}
-
       </div>
       <Footer />
     </>
   );
 };
+
 export default PrudenceRegistration;
