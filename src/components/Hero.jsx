51bb4701151086d805f1dcede9f021e128e9280f
INDJokerGamma <piyushdawkhare0000@gmail.com>
2025-12-26 19:12:57 +0530

    retriving the changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -58,13 +58,13 @@ export const Hero = ({ onExploreClick }) => {
               Welcome to <span className="font-semibold text-yellow-400">PACE</span>
             </h1>
 
-            <Link to="https://forms.gle/Rzpm4UER5dAhR3GQ6">
+            <Link to="/ComingSoon">
               <motion.button
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.97 }}
                 className="bg-yellow-400 hover:bg-yellow-300 text-black font-semibold px-6 py-3 rounded-xl transition duration-300"
               >
-                Assistant Board
+                Register for Prudence 2k25....
               </motion.button>
             </Link>
           </motion.section>
