2d9e5fb12671566b047e5ea7febfdf10b16a8182
adi-jaybhaye <jaybhayeaditya41@gmail.com>
2026-09-08 11:46:17 +0530

    Logo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,7 +30,7 @@ export const Hero = ({ onExploreClick }) => {
             className="flex justify-center items-center mb-6"
           >
             <img
-              src="/Pace_Logo.png"
+              src="/assets/Pace_Logo_White.svg"
               alt="Pace_Logo"
               className="h-23 md:h-42 w-auto object-contain" //h-20 md:h-40
             />
